6255dc9ec9cf6c7b1400ae7b50355722920f5fb1
samandarwebcreator <samandarwebcreator@gmail.com>
2024-04-11 22:14:51 +0500

    fixed the route issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LoginPage from './components/loginpage/Login';
-import MainPage from './components/mainpage/mainPageC/MainPage';
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import LoginPage from "./components/loginpage/Login";
+import MainPage from "./components/mainpage/mainPageC/MainPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/main" element={<MainPage />} />
       </Routes>
     </BrowserRouter>
